5c7348318ac141e64e7386a7ef15d366c6d5df49
Jose A. Tolentino <61167951+josetv91@users.noreply.github.com>
2021-10-29 23:49:19 -0500

    Update consolesave2.js

--- consolesave2.js
+++ consolesave2.js
@@ -27,6 +27,7 @@ for (let i=0; i<lenx;i++)
     else {
         wcl.push(" ")}
 }
+    
 //for (let i=0; i<lenx2;i++){
 for (let i=0; i<lenx;i++){
 JSON.stringify(arx.push(arr[i] + "\t" + wcl[i] + "\t" + arp[i]))//arx.push(vard[i].innerText + "\t" + phra[i].innerText)
